d327715543edf99b09f2e39dbadcc5b9ffe4c676
bjarssus <bjarssus@foxmail.com>
2017-09-11 12:19:30 +0800

    Add body-parser

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 "use strict";
 
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static("www"));
+
+app.post("/post", function(req, res, next) {
+    var foo = req.body.foo;
+    var bar = req.body.bar;
+    var baz = req.body.baz;
+    res.send({
+        success: true,
+        foo: foo,
+        bar: bar,
+        baz: baz
+    });
+})
 
 var server = app.listen(80, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Service was on http://%s:%s", host, port);
-})
+    console.log("Service on http://%s:%s", host, port);
+});
