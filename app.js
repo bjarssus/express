8de2dcd74fb6a1838073e492fd4ce63eaa29ad60
bjarssus <bjarssus@foxmail.com>
2017-10-11 16:56:33 +0800

    Modify app.js

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("www"));
 
-app.post("/post", function(req, res, next) {
-    var foo = req.body.foo;
-    var bar = req.body.bar;
-    var baz = req.body.baz;
-    res.send({
-        success: true,
-        foo: foo,
-        bar: bar,
-        baz: baz
-    });
-})
-
 var server = app.listen(config.port || 8080, function() {
     var host = server.address().address;
     var port = server.address().port;
